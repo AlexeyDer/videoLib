d893e14616c58f7c834f30a64e2f71e8853a29a0
AlexeyDer <hottelmich@gmail.com>
2020-05-20 10:53:19 +0700

    Delete link to signup

--- app.py
+++ app.py
@@ -12,10 +12,5 @@ def main():
     return render_template('index.html')
 
 
-@app.route('/showSignUp')
-def showSignUp():
-    return render_template('signup.html')
-
-
 if __name__ == '__main__':
     app.run()
