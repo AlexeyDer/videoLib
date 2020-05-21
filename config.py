411b5cdaeeeb149ff076448d7dcebbb7a0f513ee
AlexeyDer <hottelmich@gmail.com>
2020-05-22 01:15:22 +0700

    Return old settings

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
-from os import getenv
-
-
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_DATABASE_URI =
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:sport1337@localhost/videoLib'
     SECRET_KEY = '123456790'
