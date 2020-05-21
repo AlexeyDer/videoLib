6aa16c38cd9c3478a3ec6c310358d59aa4bf1098
AlexeyDer <hottelmich@gmail.com>
2020-05-22 01:10:24 +0700

    Move uri to var
    Please enter the commit message for your changes. Lines starting

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from os import getenv
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:sport1337@localhost/videoLib'
-    SQLALCHEMY_DATABASE_URI = getenv('database_url')
+    # SQLALCHEMY_DATABASE_URI =
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:sport1337@localhost/videoLib'
     SECRET_KEY = '123456790'
