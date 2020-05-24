87ce1772bc9430c5c48768087e202dea6ab61bd4
AlexeyDer <hottelmich@gmail.com>
2020-05-25 00:36:22 +0700

    Add fixes

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:sport1337@localhost/videoLib'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:password@localhost/videoLib'
     SECRET_KEY = '123456790'
