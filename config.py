d16a010ac4ac36f1bb7026f963adeb9aadf6fc51
AlexeyDer <hottelmich@gmail.com>
2020-05-28 00:45:58 +0700

    Add password

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:password@localhost/videoLib'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:passwrod@localhost/videoLib'
     SECRET_KEY = '123456790'
